76789da1a41cd730a541ca1d8c481d6c1afedf04
musabalki <musabalkimb@gmail.com>
2021-03-08 23:58:13 +0300

    Nodejs ve expressjs kurulumu yapıldı

---
+++ server.js
@@ -0,0 +1,40 @@
+const express  = require('express')
+const app = express()
+const port = 3000
+const {v4:uuidv4}=require('uuid')
+
+app.use(express.json({extended:false}))
+
+const todos=[
+    {
+        message:"message 1",
+        id:1
+    },
+    {
+        message:"message 2",
+        id:2
+    },
+    {
+        message:"message 3",
+        id:3
+    }
+]
+
+app.get('/', (req, res) => {
+    res.status(200).json(todos);
+})
+
+app.post('/', (req, res) => {
+    const newTodo=
+        {
+            message:req.body.message,
+            id:uuidv4()
+        };
+    
+    todos.push(newTodo)
+    res.status(201).json(todos);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
